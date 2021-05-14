93879f1d9f99927f47b94234d403aab1f8e2d37c
SarkarPriyanshu <provashpriyanshu@gmail.com>
2021-05-14 15:01:08 +0530

    added header and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Homepage from "./Pages/Homepage/Homepage.page";
 import Hats from "./Pages/Hats/Hats.page";
 import Shop from "./Pages/Shop/Shop.page";
+import Header from "./Components/header/header.component";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/shop" exact component={Shop} />
